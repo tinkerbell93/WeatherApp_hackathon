2239d42b432eb7e249b356a770c688bbb6bff234
= <yyb2535@gmail.com>
2020-06-05 21:42:55 +0900

    push

---
+++ js/clock.mjs
@@ -0,0 +1,8 @@
+export const obj = {
+  time: 100
+};
+export function foo() {
+  const x = 1;
+  console.log(x);
+  
+}
---
+++ js/weather.mjs
@@ -0,0 +1,3 @@
+import { obj, foo } from './clock.mjs';
+console.log(obj);
+foo();
