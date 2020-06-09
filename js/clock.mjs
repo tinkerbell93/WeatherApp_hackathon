d31fb8d03125107e8a721f9bafd045812ff4590a
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-06-10 01:14:45 +0900

    feat: modifying sate save & clear function

--- js/clock.mjs
+++ js/clock.mjs
@@ -1,6 +1,5 @@
-//state
+// state
 let customStyles = [];
-let viewClass;
 
 // DOMs
 const $main = document.querySelector('main');
@@ -12,7 +11,7 @@ const $btnSettingOpen = document.querySelector('.btn_settingOpen');
 const $setting = document.getElementById('setting');
 const $font = document.getElementById('font');
 const $fontColor = document.getElementById('font_color');
-const $bgc = document.querySelector('.container_bgc')
+const $bgc = document.querySelector('.container_bgc');
 const $state = document.querySelector('.item_state');
 const $btnSettingClose = document.querySelector('.btn_settingClose');
 
@@ -76,64 +75,14 @@ setInterval(() => {
   });
 
 
-  if($btnHoursView.classList.contains('isAct')) {
+  if ($btnHoursView.classList.contains('isAct')) {
     hour = hour % 12 || 12;
     hour = hour < 10 ? `0${hour}` : hour;
-    
+
     $time.textContent = `${ampm} ${hour}:${minute}:${second}`;
   }
-}, 1000);
+}, 100);
 
-
-// 스타일 적용
-const setStyle = () => {
-  customStyles.forEach(style => {
-    $main.style[style.name] = style.value;
-  })
-};
-
-// 스타일 상태정보 추가
-const addState = (name, value) => {
-  if (customStyles.every(style => style.name !== name)) {
-    customStyles = [{ name: name, value: value}, ...customStyles];
-  } else {
-    customStyles = customStyles.map(style => style.name === name ? {...style, name: name, value: value } : style );
-  }
-};
-
-// 초기값 설정
-const setInitStyle = () => {
-  const data = JSON.parse(localStorage.getItem('customStyles'))
-
-  const font = data.filter(style => style.name === 'fontFamily')[0].value;
-  const $selectedFont = document.querySelector(`[value = "${font}"]`);
-  const color = data.filter(style => style.name === 'color')[0].value;
-
-
-  $selectedFont.setAttribute('selected', '');
-  $fontColor.setAttribute('value', `${color}`);
-  if(localStorage.getItem('viewClass') === 'isAct') classAdd($btnHoursView, 'isAct')
-  else classRemove($btnHoursView, 'isAct');
-}
-
-
-// 리페인팅
-const paint = (name, value) => {
-  addState(name, value);
-  setStyle();
-}
-
-// 로컬스토리지에 저장되어있는 스타일 가져오기
-const getStyle = () => {
-  const data = JSON.parse(localStorage.getItem('customStyles'))
-
-  if (!data) return;
-
-  customStyles = data;
- 
-  setInitStyle();
-  paint();
-};
 
 // 클래스 토글
 const classToggle = (element, className) => {
@@ -163,38 +112,109 @@ const show = element => {
 // 선택된 요소만 클래스 추가
 const multiClassToggle = (element, className, target) => {
   [...element.children].forEach($item => $item.classList.toggle(className, $item === target));
-}
+};
+
+
+// 스타일 적용
+const setStyle = () => {
+  customStyles.forEach(style => {
+    $main.style[style.name] = style.value;
+  });
+};
+
+// 스타일 상태정보 추가
+const addState = (name, value) => {
+  if (customStyles.every(style => style.name !== name)) {
+    customStyles = [{ name, value }, ...customStyles];
+  } else {
+    customStyles = customStyles.map(style => style.name === name ? {...style, value: value } : style );
+  }
+};
+
+// 초기값 설정
+const setInitStyle = () => {
+  if (!JSON.parse(localStorage.getItem('customStyles'))) return;
+  const data = JSON.parse(localStorage.getItem('customStyles'));
+  const font = data.filter(style => style.name === 'fontFamily');
+  const color = data.filter(style => style.name === 'color');
+  const viewClass = data.filter(style => style.name === 'viewClass');
+
+  // 폰트 초기설정
+  if (font.length) {
+    const $selectedFont = document.querySelector(`[value = "${font[0].value}"]`);
+    $selectedFont.setAttribute('selected', '');
+  }
+  // 폰트 색 초기설정
+  if (color.length) $fontColor.setAttribute('value', `${color[0].value}`);
+  // hours view 설정
+  if (viewClass[0].value === 'isAct') classAdd($btnHoursView, 'isAct');
+};
+
+// 초기값 해제
+const clearInitStyle = () => {
+  if (!JSON.parse(localStorage.getItem('customStyles'))) return;
+  const data = JSON.parse(localStorage.getItem('customStyles'));
+  const font = data.filter(style => style.name === 'fontFamily');
+  const color = data.filter(style => style.name === 'color');
+  const viewClass = data.filter(style => style.name === 'viewClass');
+
+  // 폰트 초기값 해제
+  if (font.length) {
+    const $selectedFont = document.querySelector(`[value = "${font[0].value}"]`);
+    $selectedFont.removeAttribute('selected');
+  }
+  // 폰트 색 초기값 해제
+  if (color.length) $fontColor.setAttribute('value', '');
+  // hours view 초기값 해제
+  if (viewClass[0].value === 'isAct') classRemove($btnHoursView, 'isAct');
+};
+
+// 배경색 랜덤추출
+const getRandomBgc = () => {
+  const str = 'abcdef0123456789';
+  let random = '';
+
+  for (let i = 0; i < 6; i++) {
+    const count = Math.floor(Math.random() * str.length);
+
+    random += str[count];
+  }
+  return random;
+};
+
+// 로컬스토리지에 저장되어있는 스타일 가져오기
+const loadStyle = () => {
+  customStyles = JSON.parse(localStorage.getItem('customStyles')) || [];
+
+  if (!customStyles.filter(style => style.name === 'backgroundColor').length) $main.style.backgroundColor = `#${getRandomBgc()}`;
+
+  setInitStyle();
+  setStyle();
+};
 
 // 로컬스토리지에 커스텀 스타일 저장
 const saveStyle = () => {
+  if (!customStyles.length) return;
   localStorage.setItem('customStyles', JSON.stringify(customStyles));
-  localStorage.setItem('viewClass', viewClass);
-  localStorage.setItem('selectedBg', selectedBg);
-}
+};
 
 // 로컬스토리지 및 현 스타일 제거
 const clearStyle = () => {
-  const data = JSON.parse(localStorage.getItem('customStyles'))
-  if (!data) return;
-
-  const font = data.filter(style => style.name === 'fontFamily')[0].value;
-  const $selectedFont = document.querySelector(`[value = "${font}"]`);
-
-  $selectedFont.removeAttribute('selected');
-  $fontColor.setAttribute('value', ``);
-
-  localStorage.clear();
+  clearInitStyle();
   $main.style = '';
-}
+  localStorage.clear();
+};
+
 
 // eventBinding
+// 윈도우 로딩이벤트
 window.addEventListener('load', () => {
   renderInit();
-  getStyle();
+  loadStyle();
 });
 
 // setting 메뉴 열기
-$btnSettingOpen.onclick = e => {
+$btnSettingOpen.onclick = () => {
   classToggle($setting, 'active');
   hide($btnSettingOpen);
 };
@@ -203,22 +223,24 @@ $btnSettingOpen.onclick = e => {
 // 1. 마우스 이벤트
 $btnHoursView.onclick = () => {
   classToggle($btnHoursView, 'isAct');
-  viewClass = $btnHoursView.classList[$btnHoursView.classList.length - 1];
+  addState('viewClass', `${$btnHoursView.classList[$btnHoursView.classList.length - 1]}`);
 };
 // 2. 키보드 이벤트
 $btnHoursView.onkeyup = ({ keyCode }) => {
   if (keyCode === 39) classAdd($btnHoursView, 'isAct');
-  else if(keyCode === 37) classRemove($btnHoursView, 'isAct');
+  else if (keyCode === 37) classRemove($btnHoursView, 'isAct');
 };
 
 // 폰트 설정
 $font.onchange = ({ target }) => {
-  paint($font.name, target.value);
+  addState($font.name, target.value);
+  setStyle();
 };
 
 // 폰트 색 설정
 $fontColor.oninput = ({ target }) => {
-  paint($fontColor.name, target.value);
+  addState($fontColor.name, target.value);
+  setStyle();
 };
 
 // 배경 색 설정
@@ -227,15 +249,17 @@ $bgc.onchange = ({ target }) => {
   const bgColor = window.getComputedStyle(target).backgroundColor;
 
   multiClassToggle($bgc, 'select', target.previousElementSibling);
-  paint(target.name, bgColor);
+  addState(target.name, bgColor);
+  setStyle();
 };
 // 2. 키보드 이벤트
 $bgc.onkeyup = ({ keyCode, target }) => {
   if (keyCode !== 13) return;
 
-  const bgColor = window.getComputedStyle(target).backgroundColor
+  const bgColor = window.getComputedStyle(target).backgroundColor;
   multiClassToggle($bgc, 'select', target);
-  paint(target.nextElementSibling.name, bgColor);
+  addState(target.nextElementSibling.name, bgColor);
+  setStyle();
 };
 
 // 커스텀 스타일 저장 & 해제
@@ -246,7 +270,7 @@ $state.onclick = ({ target }) => {
 
 // setting 메뉴 내부 포커스 제어
 $btnSettingClose.onkeydown = e => {
-  if(e.keyCode !== 9) return;
+  if (e.keyCode !== 9) return;
 
   $btnHoursView.focus();
   e.preventDefault();
