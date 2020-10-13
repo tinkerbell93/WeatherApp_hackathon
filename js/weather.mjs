5bd2f8effaf57800cc9e452d215ad09d3965291d
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-10-13 22:26:28 +0900

    feat: modifying getJson method, credentials: 'same-origin'

--- js/weather.mjs
+++ js/weather.mjs
@@ -13,18 +13,66 @@ const $currentOption = $weather.querySelector('.current_option');
 const $weatherContents = $forecast.querySelectorAll('.weather_content');
 const $weatherIcons = $forecast.querySelectorAll('.fas.fa-spinner');
 const $temps = $forecast.querySelectorAll('.temp');
-const $dayNmaes = $forecast.querySelectorAll('.day_name');
+const $dayNames = $forecast.querySelectorAll('.day_name');
+
+// 날씨 정보 요청 api
+const getJson = fetchStr =>
+  fetch(fetchStr)
+    .then(res => res.json())
+    .then(jsonData => jsonData);
+
+// 날씨 icon render
+const weatherRender = weather => {
+  const { daily } = weather;
+  $weatherContents.forEach((_, i) => {
+    switch (daily[i].weather[0].main) {
+      case 'Thunderstorm':
+        $weatherIcons[i].className = 'wi wi-lightning';
+        break;
+      case 'Rain':
+      case 'Drizzle':
+        $weatherIcons[i].className = 'wi wi-rain';
+        break;
+      case 'Snow':
+        $weatherIcons[i].className = 'wi wi-snowflake-cold';
+        break;
+      case 'Mist':
+      case 'Smoke':
+      case 'Haze':
+      case 'Dust':
+      case 'Fog':
+      case 'Sand':
+      case 'Ash':
+      case 'Squall':
+      case 'Tornado':
+        $weatherIcons[i].className = 'wi wi-fog';
+        break;
+      case 'Clear':
+        $weatherIcons[i].className = 'wi wi-day-sunny';
+        break;
+      case 'Clouds':
+        $weatherIcons[i].className = 'wi wi-cloudy';
+        break;
+      default:
+        $weatherIcons[i].className = 'wi wi-refresh';
+    }
+    $temps[i].textContent = `${daily[i].temp.min}°C / ${daily[i].temp.max}°C`;
+    $dayNames[i].textContent = week[today + i];
+    $dayNames[0].textContent = 'Today';
+  });
+};
+
+// 도시 정보
+const cityRender = city => {
+  $currently.textContent = city.name;
+  $currentOption.textContent = city.name;
+  $currentOption.selected = true;
+};
 
 // 네비게이터를 가져올 경우
 function geoSuccess(position) {
-  const { coords } = position;
-  const { latitude, longitude } = coords;
+  const { latitude, longitude } = position.coords;
 
-  function getJson(fetchStr) {
-    return fetch(fetchStr)
-      .then(res => res.json())
-      .then(jsonData => jsonData);
-  }
   async function getWeatherAll(lat, lng) {
     $currentlyBtn.firstElementChild.className = 'fas fa-spinner';
     $currentlyBtn.disabled = true;
@@ -33,82 +81,33 @@ function geoSuccess(position) {
     const weatherData = await getJson(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`
     );
-    async function weatherRender(weather) {
-      const { daily } = weather;
-      $weatherContents.forEach((_, i) => {
-        switch (daily[i].weather[0].main) {
-          case 'Thunderstorm':
-            $weatherIcons[i].className = 'wi wi-lightning';
-            break;
-          case 'Rain':
-          case 'Drizzle':
-            $weatherIcons[i].className = 'wi wi-rain';
-            break;
-          case 'Snow':
-            $weatherIcons[i].className = 'wi wi-snowflake-cold';
-            break;
-          case 'Mist':
-          case 'Smoke':
-          case 'Haze':
-          case 'Dust':
-          case 'Fog':
-          case 'Sand':
-          case 'Ash':
-          case 'Squall':
-          case 'Tornado':
-            $weatherIcons[i].className = 'wi wi-fog';
-            break;
-          case 'Clear':
-            $weatherIcons[i].className = 'wi wi-day-sunny';
-            break;
-          case 'Clouds':
-            $weatherIcons[i].className = 'wi wi-cloudy';
-            break;
-          default:
-            $weatherIcons[i].className = 'wi wi-refresh';
-        }
-        $temps[
-          i
-        ].textContent = `${daily[i].temp.min}°C / ${daily[i].temp.max}°C`;
-        $dayNmaes[i].textContent =
-          week[
-            today + i === 7
-              ? 0
-              : today + i === 8
-              ? 1
-              : today + i === 9
-              ? 2
-              : today + i === 10
-              ? 3
-              : today + i
-          ];
-        $dayNmaes[0].textContent = 'Today';
-      });
-    }
+    console.log(weatherData);
     weatherRender(weatherData);
+
     const cityData = await getJson(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     );
-    async function cityRender(city) {
-      $currently.textContent = city.name;
-      $currentOption.textContent = city.name;
-      $currentOption.selected = true;
-    }
     cityRender(cityData);
-    const koreaCityData = await getJson('../json/cityKR.list.json');
-    $citySelect.addEventListener('change', async ({ target }) => {
+
+    const koreaCityData = await getJson('../json/cityKR.list.json', {
+      credentials: 'same-origin',
+    });
+
+    $citySelect.addEventListener('change', ({ target }) => {
       const cityObj = koreaCityData.filter(city => city.name === target.value);
-      const { coord } = cityObj[0];
-      const { lat, lon } = coord;
+      const { lat, lon } = cityObj[0].coord;
       $weatherIcons.forEach(icon => {
         icon.className = 'fas fa-spinner';
       });
+
       getWeatherAll(lat, lon);
     });
+
     $currentlyBtn.firstElementChild.className = '';
     $currentlyBtn.disabled = false;
     $citySelect.disabled = false;
   }
+
   getWeatherAll(latitude, longitude);
 
   $currentlyBtn.addEventListener('click', () => {
@@ -125,8 +124,8 @@ function geoError() {
 function askForCoords() {
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 }
+
 // 초기 설정
-function init() {
+window.addEventListener('load', () => {
   askForCoords();
-}
-init();
+});
